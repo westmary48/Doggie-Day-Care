1ee4fa150c497ae26c2121e361bd4e2f0edf2ab0
Mary West <westmarye1219@gmail.com>
2019-06-18 21:27:32 -0500

    WIP

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 
+import dogs from './dogs';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 function App() {
+  state = {
+    dogs: [],
+  }
+
+  componentDidMount() {
+    this.setState({ dogs });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className='btn btn-danger'>HELP ME</button>
-      </header>
+      <div>Doggie Daycare</div>
     </div>
   );
 }
