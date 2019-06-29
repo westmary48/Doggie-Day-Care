44b0e3523f4b2a3b1fc6d27623e67c89d012ce79
Mary West <westmarye1219@gmail.com>
2019-06-29 11:37:10 -0500

    added form with drop downs

--- src/components/WalksForm/WalksForm.js
+++ src/components/WalksForm/WalksForm.js
@@ -3,17 +3,45 @@ import React from 'react';
 import './WalksForm.scss';
 
 class WalksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+  }
+
   render() {
     return (
       <div className="WalksForm">
         <form>
-          <h1>Add New Walk</h1>
-          <select class="form-control">
-            <option>Employees</option>
+        <label>
+          Employees:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="Pam">Pam</option>
+            <option value="Dwight">Dwight</option>
+            <option value="Jim">Jim</option>
+            <option value="Micheal">Michael</option>
+            <option value="Angela">Angela</option>
           </select>
-          <select class="form-control">
-            <option>Dogs</option>
+        </label>
+        <label>
+          Dogs:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="Cooper">Cooper</option>
+            <option value="Bentley">Bentley</option>
+            <option value="Missy">Missy</option>
+            <option value="Sam">Sam</option>
+            <option value="Tucker">Tucker</option>
           </select>
+        </label>
           <label>
             Date:
             <input type="text" name="name" />
