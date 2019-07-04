351a6ed606dded4cf496769aba42c8f90c6814ea
Mary West <westmarye1219@gmail.com>
2019-07-04 16:49:36 -0500

    fixed dog pen import

--- src/components/DogPen /DogPen.js
+++ src/components/DogPen /DogPen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Dog from '../../Dog/Dog';
+import Dog from '../Dog/Dog';
 
 import dogShape from '../../helpers/propz/dogShape';
 
